b529262322d9de5ba5bdc24a5edfe0858e3853ac
runtee <ekwo.chinonso.01@gmail.com>
2022-09-16 10:08:27 +0100

    added names

--- zoom.py
+++ zoom.py
@@ -2,24 +2,55 @@ from selenium import webdriver
 import time
 from selenium.webdriver.common.by import By
 from random import randint
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import NoSuchElementException
+
 
 
 import csv
+nameFile =  open('./name.csv','r')
+nameDict= csv.DictReader(nameFile,delimiter=',')
+nameList = []
+for row in nameDict:
+    nameList.append(row)
+nameList
 file =  open('./details.csv','r')
 mycsv= csv.DictReader(file,delimiter=',')
 for row in mycsv:
         mycsv= row
 url = 'https://us05web.zoom.us/wc/join/'+str(mycsv['meetingID'])+'?pwd='+str(mycsv['password'])
-
 def join(url):
-    driver = webdriver.Chrome('/usr/bin/chromedriver')
-    driver.get(url)
-    time.sleep(5)
-    text_box = driver.find_element(by=By.NAME, value="inputname")
-    submit_button = driver.find_element(by=By.ID, value="joinBtn")
-    text_box.send_keys("user1"+str(randint(1,100000)))
-    submit_button.click()
+        options = Options()
+        options.headless   = True
+        driver = webdriver.Chrome('/usr/bin/chromedriver', options=options)
+        driver.get(url)
+        time.sleep(5)
+        title = driver.title
+        print(title)
+        if ("Zoom" in title):
+                try:    
+                        text_box = driver.find_element(by=By.NAME, value="inputname")
+                        submit_button = driver.find_element(by=By.ID, value="joinBtn")
+                        text_box.send_keys(str(nameList[randint(0,len(nameList))]['names']).capitalize())
+                        submit_button.click()
+                        time.sleep(10)
+                        if('Error' in driver.title):
+                                print('This meeting link has expired or invalid')
+                                return 'This meeting link has expired or invalid'
+                        else:
+                                print('joined meeting')
+                                time.sleep(10000)
 
+                                return('joined meeting')
+
+
+                except NoSuchElementException:
+                        print('An error occur, the page might have been changed') 
+                        return                       
+        else:
+                print('An error occur, the page might have been changed')
+                return
+           
 join(url)
 
 # time.sleep(10000)
